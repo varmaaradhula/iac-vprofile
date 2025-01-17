4470aac4abdbe7b7a87b94ee1bf8af9114f7d828
varmaaradhula <rkvarmaa@gmail.com>
2025-01-17 21:13:13 +0000

    changed names

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,10 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_availability_zones" "available" {}
+data "aws_availability_zones" "available" {
+
+}
+
 
 locals {
   cluster_name = var.clusterName
